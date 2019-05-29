3f480275f793cdf49ec8fce529232966789281bf
typicalwestern <38580563+typicalwestern@users.noreply.github.com>
2019-05-29 15:07:56 +0300

    Update pyton lab2.1.py

--- pyton lab2.1.py
+++ pyton lab2.1.py
@@ -1,6 +1,11 @@
-import re
+rez = []
 string = input('Введите строку ')
-
-result = re.findall(r'\w+ов', string)
-print(result)
-
+replacements = (',', '-', '!', '?', '.', ':')
+for r in replacements:
+    string = string.replace(r, ' ')
+words = string.split()
+for i in range(0,len(words)):
+    c = len(words[i])
+    if words[i][c-2:c] == 'ов':
+        rez.append(words[i])
+print(rez)
